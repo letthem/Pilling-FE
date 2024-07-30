346cda6bb28134d3e5860da232d24be9dfde5e6b
letthem <suyeon3662@naver.com>
2024-07-30 20:39:36 +0900

    Fix: 중앙 정렬

--- src/components/PLFrame.js
+++ src/components/PLFrame.js
@@ -5,7 +5,7 @@ export const PLFrame = styled.div`
   background-color: white;
   max-width: 32rem;
   height: auto;
-  margin: auto;
+  margin: 0 auto;
   display: flex;
   flex-direction: column;
 `;
