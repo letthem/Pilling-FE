8a667b9a1111d0971f3320c83e3a8f248386b1ed
letthem <suyeon3662@naver.com>
2024-08-04 11:30:27 +0900

    Feat: 복용기록 GET

--- src/api/api.js
+++ src/api/api.js
@@ -8,3 +8,14 @@ export const axiosInstance = axios.create({
     'Content-Type': 'application/json',
   },
 });
+
+axiosInstance.interceptors.request.use(
+  config => {
+    const token = localStorage.getItem('access_token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  error => Promise.reject(error)
+);
