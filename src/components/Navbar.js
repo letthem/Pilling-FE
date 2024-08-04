cce9528ed49e31a3ce8bbe55d40cc07461a6fb66
letthem <suyeon3662@naver.com>
2024-08-04 15:32:27 +0900

    Design: Navbar 디자인 수정

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,14 @@
-import {  NavLink, useLocation } from 'react-router-dom';
-import styled from 'styled-components';
-import CalLogo from '../assets/Nav/CalLogo.svg'
-import HomeLogo from '../assets/Nav/HomeLogo.svg'
-import MapLogo from '../assets/Nav/MapLogo.svg'
-import Profile from '../assets/Nav/Profile.svg'
- 
+import { NavLink, useLocation } from "react-router-dom";
+import styled from "styled-components";
+import CalLogo from "../assets/Nav/CalLogo.svg";
+import HomeLogo from "../assets/Nav/HomeLogo.svg";
+import MapLogo from "../assets/Nav/MapLogo.svg";
+import Profile from "../assets/Nav/Profile.svg";
 
 const Navbar = () => {
   const location = useLocation();
   const isProfilePage = location.pathname === "/profile";
-  
+
   return (
     <NavContainer>
       <Container isProfilePage={isProfilePage}>
@@ -17,26 +16,26 @@ const Navbar = () => {
           <StyledNavLink to="/home">
             <Icon src={HomeLogo} />
           </StyledNavLink>
-        </MeunItem>  
+        </MeunItem>
         <MeunItem>
           <StyledNavLink to="/map">
             <Icon src={MapLogo} />
           </StyledNavLink>
-        </MeunItem>  
+        </MeunItem>
         <MeunItem>
-          <StyledNavLink to="/calendar"
-            ><Icon src={CalLogo} />
-          </StyledNavLink>                    
+          <StyledNavLink to="/calendar">
+            <Icon src={CalLogo} />
+          </StyledNavLink>
         </MeunItem>
         <MeunItem>
           <StyledNavLink to="/profile">
-            <Icon src={Profile} />          
-           </StyledNavLink>                 
-        </MeunItem>  
+            <Icon src={Profile} />
+          </StyledNavLink>
+        </MeunItem>
       </Container>
     </NavContainer>
-  )
-}
+  );
+};
 
 export default Navbar;
 
@@ -45,53 +44,49 @@ const NavContainer = styled.div`
   justify-content: center;
   width: 100%;
   align-items: center;
-  height: 102px;
   box-sizing: border-box; /* Include padding in the total width calculation */
   z-index: 1;
-  `
+`;
 
 const Container = styled.ul`
   display: flex;
-  justify-content: space-evenly;
+  justify-content: space-around;
   align-items: center;
   list-style: none;
-  max-width: 460.8px; 
+  max-width: 442px;
   min-width: 325px;
-  width: 90%;
-  height: 56px;
+  width: calc(100% - 70px);
+  height: 48px;
   border-radius: 40px;
   background: ${({ isProfilePage }) => (isProfilePage ? "#ffffff" : "#f7f6f9")};
   box-shadow: 0px 0px 4px 0px rgba(115, 123, 152, 0.25);
   position: fixed;
   bottom: 14px;
-  padding: 0 24px; /* Add padding to both sides */
-  box-sizing: border-box; /* Ensure padding is included in width calculation */  
-  `
-  
+  margin: 0 24px;
+  padding: 0 11px;
+`;
+
 const MeunItem = styled.li`
   display: flex;
-  justify-content: center; 
-  align-items: center; 
-`
+  justify-content: center;
+  align-items: center;
+`;
 
 const StyledNavLink = styled(NavLink)`
   text-decoration: none;
   color: #000;
   display: flex;
   justify-content: center;
-  width: 50px;
-  height: 100%;
-  padding: 10px;
+  width: 60px;
+  height: 32px;
   border-radius: 20px;
 
   &.active {
-    background-color: #C4F260;
-    
+    background-color: #c4f260;
   }
 `;
 
 const Icon = styled.img`
-  width: 20px; 
-  height: auto; 
+  width: 20px;
+  height: auto;
 `;
-
